95cc9bd54f3af14212c6d527b642a08b2e55b6bf
misskiyeon <trapezer84@hotmail.com>
2016-01-20 17:23:44 +0900

    edit static variable

--- Assignment/Day8/src/com/ktds/leinalee/Main.java
+++ Assignment/Day8/src/com/ktds/leinalee/Main.java
@@ -9,39 +9,34 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		Solution solution = new Solution();
 		
-		//문제 1번을 위한 변수
-		String name="";
-		String address="";
-		int age;
-		
 		//문제 2번을 위한 변수
-		String operatorTwo="";
+		String operatorOne="";
 		
 		//문제 3번을 위한 변수
 		int numberOne;
 		int numberTwo;
-		String operatorThree;
+		String operatorTwo;
 		String exitCheck;
 		
 		//문제 1번 
 		System.out.println("<<인사하는 메소드 만들기>>");
 		System.out.print("이름을 입력해주세요 :");
-		name = scanner.next();
+		Solution.name = scanner.next();
 		
 		System.out.print("주소을 입력해주세요 :");
-		address = scanner.next();
+		Solution.address = scanner.next();
 		
 		System.out.print("나이을 입력해주세요 :");
-		age = scanner.nextInt();
+		Solution.age = scanner.nextInt();
 		
-		solution.sayHello(name, address, age);
+		solution.sayHello();
 		
 		//문제 2번 
 		System.out.println("<<사용자가 입력한 연산자 알아보기>>");
 		System.out.print("연산자를 입력하세요 :");
-		operatorTwo = scanner.next();
-		operatorTwo = solution.catchOperator(operatorTwo);
-		System.out.println("사용자가 입력한 연산자는 " + operatorTwo + " 입니다.");
+		operatorOne = scanner.next();
+		operatorOne = solution.catchOperator(operatorOne);
+		System.out.println("사용자가 입력한 연산자는 " + operatorOne + " 입니다.");
 		
 		//문제 3번
 		while(true) {
@@ -53,9 +48,9 @@ public class Main {
 			numberTwo = scanner.nextInt();
 			
 			System.out.print("연산자를 입력하세요(+ - * /)  :");
-			operatorThree = scanner.next();
+			operatorTwo = scanner.next();
 			
-			solution.computer(numberOne, numberTwo, operatorThree);
+			solution.computer(numberOne, numberTwo, operatorTwo);
 			
 			System.out.print("exit을 입력하면 종료됩니다.");
 			exitCheck = scanner.next();
--- Assignment/Day8/src/com/ktds/leinalee/Solution.java
+++ Assignment/Day8/src/com/ktds/leinalee/Solution.java
@@ -1,18 +1,28 @@
 package com.ktds.leinalee;
 
 public class Solution {
+	
+	public static String name="";
+	public static String address="";
+	public static int age;
+	
 	/**
 	 * 문제 1번, 인사하는 메소드
 	 * @param name
 	 * @param address
 	 * @param age
 	 */
-	public void sayHello (String name, String address, int age) {
+	public void sayHello () {
 		System.out.println("안녕하세요. 저는 " + name + "입니다.\n"
 				+ "사는 곳은 " + address + "입니다.\n"
 						+ "나이는 "+ age + "세 입니다.");
 	}
 	
+	/**
+	 * 문제 2번, 연산자 알아보기
+	 * @param userInput
+	 * @return
+	 */
 	public String catchOperator ( String userInput ) {
 		
 		if ( userInput.equals("+") )
@@ -27,6 +37,13 @@ public class Solution {
 			return "해당하는 연산자가 없습니다.";
 	}
 	
+	
+	/**
+	 * 문제 3번, 사칙연산 메소드
+	 * @param numberOne
+	 * @param numberTwo
+	 * @param operator
+	 */
 	public void computer ( int numberOne, int numberTwo, String operator ) {
 		
 		double result;
